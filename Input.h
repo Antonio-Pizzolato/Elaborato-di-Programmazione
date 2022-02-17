0059af63c8bbd5370386a9c36a9429f8b5316753
Zhengjunliang <junliang.zheng@stud.unifi.it>
2022-02-17 23:25:48 +0100

    game update

---
+++ Input.h
@@ -0,0 +1,32 @@
+#ifndef SFML_TEMPLATE_INPUT_H
+#define SFML_TEMPLATE_INPUT_H
+
+class Input
+{
+public:
+
+    /**
+     * An enum denoting all possible input keys.
+     */
+    enum class KEY {
+        KEY_LEFT,
+        KEY_RIGHT,
+        KEY_UP,
+        KEY_DOWN,
+        KEY_ATTACK,
+        KEY_DASH,
+        KEY_ESC,
+        KEY_SPACE,
+        KEY_I,
+        KEY_P
+    };
+
+    /**
+     * Checks if a given key is currently pressed.
+     * @param keycode The key to check.
+     * @return True if the given key is currently pressed.
+     */
+    static bool IsKeyPressed(KEY keycode);
+};
+
+#endif //SFML_TEMPLATE_INPUT_H
--- cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/DependInfo.cmake
@@ -1,23 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/Components/AnimationComponent.cpp" "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/Components/AnimationComponent.cpp.obj"
-  "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/Components/HitboxComponent.cpp" "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/Components/HitboxComponent.cpp.obj"
-  "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/Components/MovementComponent.cpp" "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/Components/MovementComponent.cpp.obj"
-  "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/Entities/Entity.cpp" "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/Entities/Entity.cpp.obj"
-  "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/Entities/Player.cpp" "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/Entities/Player.cpp.obj"
-  "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/GUI.cpp" "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/GUI.cpp.obj"
-  "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/GUI/PauseMenu.cpp" "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/GUI/PauseMenu.cpp.obj"
-  "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/Game.cpp" "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/Game.cpp.obj"
-  "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/GraphicsSettings.cpp" "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/GraphicsSettings.cpp.obj"
-  "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/States/GameState.cpp" "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/States/GameState.cpp.obj"
-  "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/States/MainMenuState.cpp" "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/States/MainMenuState.cpp.obj"
-  "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/States/SettingState.cpp" "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/States/SettingState.cpp.obj"
-  "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/States/State.cpp" "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/States/State.cpp.obj"
-  "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/main.cpp" "C:/Users/Antonio/CLionProjects/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/main.cpp.obj"
+  "D:/C++/Elaborato-di-Programmazione/Components/AnimationComponent.cpp" "D:/C++/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/Components/AnimationComponent.cpp.obj"
+  "D:/C++/Elaborato-di-Programmazione/Components/HitboxComponent.cpp" "D:/C++/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/Components/HitboxComponent.cpp.obj"
+  "D:/C++/Elaborato-di-Programmazione/Components/MovementComponent.cpp" "D:/C++/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/Components/MovementComponent.cpp.obj"
+  "D:/C++/Elaborato-di-Programmazione/Entities/Entity.cpp" "D:/C++/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/Entities/Entity.cpp.obj"
+  "D:/C++/Elaborato-di-Programmazione/Entities/Player.cpp" "D:/C++/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/Entities/Player.cpp.obj"
+  "D:/C++/Elaborato-di-Programmazione/GUI.cpp" "D:/C++/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/GUI.cpp.obj"
+  "D:/C++/Elaborato-di-Programmazione/GUI/PauseMenu.cpp" "D:/C++/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/GUI/PauseMenu.cpp.obj"
+  "D:/C++/Elaborato-di-Programmazione/Game.cpp" "D:/C++/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/Game.cpp.obj"
+  "D:/C++/Elaborato-di-Programmazione/GraphicsSettings.cpp" "D:/C++/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/GraphicsSettings.cpp.obj"
+  "D:/C++/Elaborato-di-Programmazione/Input.cpp" "D:/C++/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/Input.cpp.obj"
+  "D:/C++/Elaborato-di-Programmazione/main.cpp" "D:/C++/Elaborato-di-Programmazione/cmake-build-debug/CMakeFiles/PROJECT_NAME.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
@@ -28,7 +29,11 @@ set(CMAKE_TARGET_DEFINITIONS_CXX
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "C:/mingw64/include"
+  "C:/x86_64-7.3.0-release-posix-seh-rt_v5-rev0/mingw64/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
   )
 
 # Targets to which this target links.
