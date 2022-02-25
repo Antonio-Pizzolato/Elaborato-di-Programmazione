ab9238bc4ca5da03ad74a0abdfcb109caf1224c0
Antonio-Pizzolato <antonio.pizzolato@stud.unifi.it>
2022-02-25 21:28:35 +0100

    Collision Test

--- test/CauseCollisionTest.cpp
+++ test/CauseCollisionTest.cpp
@@ -5,14 +5,23 @@ class WarriorSuite : public ::testing::Test {
 
 protected:
     void SetUp() override {
-
+        sf::Texture texture;
+        texture.loadFromFile("Resources/Images/Sprites/Player/texture_sheet.png");
+        level = Level(window);
+        warrior = new Warrior(texture, PLAYER_CLASS::WARRIOR);
     }
     Player* warrior;
     sf::RenderWindow window;
     Level level;
 };
 
-TEST_F(WarriorSuite, CauseCollisionTest){
-
+TEST_F(WarriorSuite, CauseCollisionTest) {
+    level.GenerateLevel();
+    warrior->SetPosition(level.SpawnLocation());
+    warrior->SetUp();
+    for (int x = 0; x < 5; x++) {
+        warrior->Update(10, level);
+        EXPECT_EQ(warrior->GetPreviousPosition(), warrior->GetPosition());
+    }
 }
 
