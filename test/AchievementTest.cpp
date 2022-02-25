3800d78db2d8ff5a673cf24402988de2e97db859
Antonio-Pizzolato <antonio.pizzolato@stud.unifi.it>
2022-02-25 02:28:15 +0100

    Achievement Test

--- test/AchievementTest.cpp
+++ test/AchievementTest.cpp
@@ -4,6 +4,10 @@
 
 TEST(AchievementTest, UnlockAchievement){
 
+    ConditionAchievement conditionAchievement;
+    auto achievements = new Achievements(&conditionAchievement);
+    achievements->update(3, 15);
+    ASSERT_TRUE(achievements->getKill());
 
 }
 
